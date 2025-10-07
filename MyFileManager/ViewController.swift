10ce1e9ebd69bc73c5f3c752e57c6e256eeb05b5
Aleksandr Mosolov <agmosolov@gmail.com>
2025-10-07 18:32:39 +0300

    Initial Commit

---
+++ MyFileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFileManager
+//
+//  Created by Александр Мосолов on 07.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
